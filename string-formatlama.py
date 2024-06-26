b5f4c535613db9002308144d026beb76879bced9
Melike Özkan <115733605+mellikeozkan@users.noreply.github.com>
2024-06-27 00:30:36 +0300

    Add files via upload

--- string-formatlama.py
+++ string-formatlama.py
@@ -4,6 +4,15 @@ soyad = "Özkan"
 yas = 22
 
 # msj ="My name is " + ad + " " + soyad +"." + "I'm " + str(yas)+ " years old." My name is Melike Özkan.I'm 22 years old.Çıktısını verir.
+# print(msj)
 
 
-print(msj)
+# STRİNG FORMAT
+# msj = "My name {0} {1}. I'm {2} years old .".format(ad,soyad,yas)
+# print(msj)   # formattan sonraki sıraya 0-1-2 verdik .parantezle  istediğimiz gibi sıralayabiliriz .bunun yeine şunu da yapabilirdik .
+# msj = "My name {a} {s}. I'm {y} years old .".format(a=ad,s=soyad,y=yas)
+
+
+# F-STRİNG
+msj = f"My name {ad} {soyad} . I'm {yas} years old."
+print(msj)  # Burada direkt olarak üstte yazılan parametreleri kullanabiliriz.
---
+++ string-uygulamaları.py
@@ -0,0 +1,31 @@
+title = "Python ile programlama Dersleri"
+ 
+#1-'title' değişkeni içerisindeki karakter sayısı nedir?
+# 2-title içersindeki Python kelimesini alın 
+# 3- title değişkeninin ilk 5 ve son 5 karakterini alın.
+# 4- title değişkenini tersten yazdırın 
+# 5- klavyeden girilen öğrenci bilgisine göre örnek verilen cümleyi yazdırın.
+#örnek : Çınar turan isimli öğrencinin 1.notu 60,2.notu 60 ve not ortalaması 60 olarak hesaplanmıştır.
+
+#1. UYGULLAMA  
+
+# adet = len(title)
+# print(adet)      # 31 çıktısını verdi 
+
+
+# 2.UYGULAMA 
+# print(title[:7])   ekrana python kelimesini yazdırdık.
+
+
+# 3.UYGULAMA 
+# print(title[:5])   PYTHO
+# print(title[-5:])  SLERİ
+
+# 4.UYGULAMA 
+# print(title[ : :-1])  irelsreD amalmargorp eli nohtyP
+
+
+# 5.UYGULAMA 
+isim ="Çınar Turan"
+msj = f"{isim} adlı öğrencinin 1.notu 60 ,2.notu 60 ve not ortalaması 60 olarak hesaplanmıştır."
+print(msj)
